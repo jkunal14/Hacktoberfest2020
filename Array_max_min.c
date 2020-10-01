9ee8167affa4dedef412ed9754300e34b530f270
jkunal14 <43882559+jkunal14@users.noreply.github.com>
2020-10-01 18:15:57 +0530

    Update Array_max_min.c

--- Array_max_min.c
+++ Array_max_min.c
@@ -3,29 +3,28 @@
 int maximum(int A[], int n){
     int mx = A[0];
     for (int i = 1; i < n; i++)
-    {
-        if(mx < A[i]){
+        if(mx < A[i])
             mx = A[i];
-        }
-    }
     return mx;
-    
 }
 int minimum(int A[], int n){
     int mn = A[0];
     for (int i = 1; i < n; i++)
-    {
-        if(mn > A[i]){
+        if(mn > A[i])
             mn = A[i];
-        }
-    }
     return mn;
-    
 }
 
 int main(){
-    int A[] = {1, 3, 4, 1}; 
-    int size = (sizeof(A)/sizeof(int));
+    int n;
+    printf("Enter Size of Array :");
+    scanf("%d", &n);
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        scanf("%d", &arr[i]);
+    }
+    
     int min = minimum(A, size);
     // printf("%d\n",min);
     int max = maximum(A, size);
